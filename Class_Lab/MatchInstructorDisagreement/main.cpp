48d1124564f6037d70ea1452f08297b9db8f38c8
ny2554415 <nyounus@student.rcc.edu>
2015-01-07 11:30:11 -0800

    Signed-off-by: ny2554415 <nyounus@student.rcc.edu>

---
+++ Class_Lab/MatchInstructorDisagreement/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Nabil Younus
+ * Created on January 7, 2015, 11:17 AM
+ * Purpose: How easy computers can make mistakes
+ */
+ //System Libraries
+#include <iostream>
+using namespace std;
+
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Executions begin here
+
+
+int main(int argc, char** argv) {
+//Declare variables
+float a,b,c;
+//Prompt for a and b
+cout<<"Input 2 short values"<<endl;
+cin>>a>>b;
+//Sum the values
+c=a+b;
+//Output the results
+cout<<c<<" = "<<a<<" + "<<b<<endl;
+//Exit stage right
+
+    return 0;
+}
+
